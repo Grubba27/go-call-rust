ae963c80c33ffb83bffe204d5fe09058113dad4d
Gabriel Grubba <70247653+Grubba27@users.noreply.github.com>
2023-04-12 00:32:11 -0300

    Update main.rs

--- rust-code/src/main.rs
+++ rust-code/src/main.rs
@@ -8,15 +8,11 @@ struct Args {
     #[arg(short, long)]
     name: String,
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
 }
 
 fn main() {
     let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello, {}!", args.name)
-    }
+    
+    println!("Hello, {}!", args.name)
+    
 }
